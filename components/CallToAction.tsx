af19d451208ca8829cc1414bd6b435a73fa4ee8d
Yash <yashlaxman@gmail.com>
2025-09-06 13:11:45 +0530

    update ui content and styles

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function CallToAction() {
   const router = useRouter();
   return (
-    <motion.div className="absolute bottom-1/5 w-screen space-y-4 pt-8">
+    <motion.div className="absolute bottom-2/7 w-screen space-y-4 pt-8 sm:bottom-1/5">
       <div className="relative mx-auto w-full max-w-4xl overflow-hidden">
         <div className="pointer-events-none absolute top-0 left-0 z-10 h-full w-12 bg-gradient-to-r from-black to-transparent" />
         <div className="pointer-events-none absolute top-0 right-0 z-10 h-full w-12 bg-gradient-to-l from-black to-transparent" />
